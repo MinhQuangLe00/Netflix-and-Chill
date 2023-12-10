8171c734ed80a0d7932e575509273536e8655eeb
MinhQuangLe00 <lenguyenminhquang01062000@gmail.com>
2023-12-11 01:13:09 +0700

    1st commit

---
+++ MoviePenguin/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿
+using MoviePenguin.DAO;
+using MoviePenguin.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace MoviePenguin.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            TestDBContext test = new TestDBContext();
+            var x = test.Countries.ToList();
+            var MovieDao = new MovieDao();
+            ViewBag.listMovieNew = MovieDao.ListMovieNew(6);
+            ViewBag.listMovieTop = MovieDao.ListMovieTop(4);
+            ViewBag.listMoviePo = MovieDao.ListMoviePo(4);
+            ViewBag.Slides = new SlideDao().ListAllSlide(5);
+            return View(MovieDao);
+        }
+        public ActionResult Search()
+        {
+            return View();
+        }
+    }
+}
